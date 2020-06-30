12f4412f49738c5d34565d334ce92f7e04aa3654
= <1276218716@qq.com>
2020-06-30 11:19:42 +0800

    del

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
+
+    public GameObject A, B;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class Gamemanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        A.transform.position = A.transform.position - new Vector3(Time.deltaTime, 0, 0);
+        B.transform.position = B.transform.position + new Vector3(Time.deltaTime, 0, 0);
+        if (A.GetComponent<BoxCollider>().isTrigger)
+        {
+            Debug.Log("yes");
+        }
     }
 }
