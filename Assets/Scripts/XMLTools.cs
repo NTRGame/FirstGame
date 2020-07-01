116bd5391f2cefce4686eaa97847099da64f1446
= <1276218716@qq.com>
2020-07-01 21:05:52 +0800

    XmlToolMode

---
+++ Assets/Scripts/XMLTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class XMLTools
+{
+    public static Soldier GetSoldierByType(SoldierType soldierType,PlayerType player)
+    {
+        return new Soldier(10f, 3f, 3, soldierType, 3f) { playerType = player };
+    }
+}
