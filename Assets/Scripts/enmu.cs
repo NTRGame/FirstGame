86742453bff12318f8a3f27c0e93884af9eb81a1
= <1276218716@qq.com>
2020-07-01 21:05:37 +0800

    Enmu Type

---
+++ Assets/Scripts/enmu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum SoldierType
+{
+    FlagZombie
+}
+
+public enum PlayerType
+{
+    Left,Right
+}
